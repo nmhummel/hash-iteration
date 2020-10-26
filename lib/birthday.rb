f8ae9c26dc32da9ca6fe216c46ee101cade60a69
nmhummel <natalie.hummel@gmail.com>
2020-10-26 15:37:37 -0500

    commit tree 72d434c5302c8012404fe34e2309c535e4299d30

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,7 +5,25 @@
 # }
 
 def happy_birthday(birthday_kids)
-  # add your code snippet here!
+  birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
+end 
+
+# Here we are using #each to iterate over each pair of kids name/age. We are yielding the key/value pair to the 
+# block of code between the do/end keywords by assigning # the variables kids_name and age in between the pipes, | |, 
+# to be the placeholders for each key/value pair.
+
+# Then, we can use those variable names in our string interpolation to puts out the actual values 
+# they point to at each step of the iteration.
+
+
+def age_appropriate_birthday(birthday_kids)
+  birthday_kids.each do |kids_name, age|
+    if age <= 12 
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    else 
+      puts "You are too old for this."
+    end
+  end
 end
-
-
--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,5 +7,19 @@
 # }
 
 def select_winner(passengers)
-  # add the code snippet here!
+  winner = ""
+  passengers.each do |suite, name|
+    if suite == :suite_a && name.start_with?("A")
+      winner = name
+    end
+  end
+  
+  winner 
 end
+
+# We chose #each instead of collect because we don't want to collect the key/value pair that contains the winner, just the name of the winner. 
+# With #each, we have the control we need to simply grab the winner's name and set it equal to a variable that we can return later on.
+# we use an if statement combined with the && ("and") boolean operator to check if we have the right suite and if the person in that
+# suite has a name that begins with the letter "A".
+# If that condition returns true, we've found our winner! We set their name equal to the winner variable and end our iteration.
+# Then, we call on our winner variable to return the name of the lucky winner.
